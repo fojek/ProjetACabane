af4255be6a53c6f25e330c8f2555c49eb2f66eb4
fojek <marc.fortier.1@gmail.com>
2015-11-17 10:46:33 -0500

    Update global.hpp

--- global.hpp
+++ global.hpp
@@ -1,9 +1,8 @@
+// Variables globales
+
 #ifndef GLOBAL_HPP
 #define GLOBAL_HPP
 
-extern long int echoStart;
-extern long int echoEnd;
-extern long int echoDuration;
 extern int bassin2Level;
 
 #endif // GLOBAL_HPP
